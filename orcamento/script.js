ab8414d1be29edf4270b887d86abe3db1c2ab06b
Matheus Gonzaga <143510485+Matheusjjk3@users.noreply.github.com>
2024-01-10 12:08:57 -0300

    Add files via upload

---
+++ orcamento/script.js
@@ -0,0 +1,20 @@
+var enviar = document.getElementById("enviar")
+var resultado = document.getElementById("txtMedia")
+
+enviar.addEventListener("click", function (event) {
+  var altura01 = document.getElementById("altura").value
+  var largura01 = document.getElementById("largura").value
+  var select = document.getElementById("select")
+  var operacao = select.options[select.selectedIndex].value
+  var result = 0
+
+  if (operacao == "branco") {
+    result = (altura01 * largura01 * 850) / 1000000
+  } else if (operacao == "amadeirado") {
+    result = (altura01 * largura01 * 1000) / 1000000
+  } else {
+    result = (altura01 * largura01 * 850) / 1000000
+  }
+  document.getElementById("txtMedia").innerHTML =
+    "Valor: R$" + result.toFixed(2).replace(".", ",")
+})
